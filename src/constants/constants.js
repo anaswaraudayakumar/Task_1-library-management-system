0ee3bdac28bdce2261a0defa217faa0de9f55c00
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-07 10:31:20 +0530

    chore(constant):"add new user in the array"

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 
 const EMAILREGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const PASSWORD_MIN_LENGTH = 6
-const USERTYPES = ["admin","member"]
+const USERTYPES = ["admin","member","librarian"]
 
 module.exports={EMAILREGEX,PASSWORD_MIN_LENGTH,USERTYPES}
