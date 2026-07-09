19efd8869e0322a40b3c0158651424adef17db70
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-09 16:07:43 +0530

    feat:librarian middleware added

---
+++ src/middleware/librarianMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const STATUS_CODES = require("../constants/statusCodes");
+const MESSAGES = require("../constants/messages");
+
+function librarianMiddleware(req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+  // console.log(token);
+  if (token) {
+    try {
+      const jwtResponse = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(jwtResponse);
+      // req.payload = jwtResponse._id;
+       const role = jwtResponse.role;
+      if (role == "librarian") {
+        next();
+      } else {
+        res.status(STATUS_CODES.UN_AUTHORIZED).json(MESSAGES.INV_TOKEN);
+      }
+    } catch (error) {
+      res.status(STATUS_CODES.UN_AUTHORIZED).json(MESSAGES.INV_TOKEN);
+    }
+  } else {
+    //MESSAGES
+    res.status(STATUS_CODES.UN_AUTHORIZED).json(MESSAGES.AUTH_FAIL);
+  }
+}
+module.exports = librarianMiddleware
