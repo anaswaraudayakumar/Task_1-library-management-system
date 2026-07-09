146d49ae9b465fa2d835b9da4f34c213337775e8
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-09 16:13:26 +0530

    feat(author):add controll file for author

---
+++ src/controller/authorController.js
@@ -0,0 +1,52 @@
+const {createAuthorService, findAllAuthorService} = require("../services/authorService")
+const MESSAGES = require("../constants/messages");
+const STATUS_CODES = require("../constants/statusCodes");
+const { getAll } = require("../repositories/userRepository");
+
+
+async function createAuthorController(req,res){
+    console.log("Inside createAuthorController ");
+    try {
+        const author = await createAuthorService(req.body)
+        res.status(STATUS_CODES.CREATED).json({
+            success:true,
+            message:MESSAGES.AUTHOR_CRE_SUCCESS,
+            data:author
+        })
+    } catch (error) {
+        res.status(STATUS_CODES.BAD_REQUEST).json({
+            success:false,
+            message:error.message
+        })
+    }
+    
+    
+}
+//getallauthor
+async function getAllAuthorController(req,res){
+    console.log("Inside getAllAuthorController ");
+    try {
+        const author = await findAllAuthorService(req.query)
+        res.status(STATUS_CODES.OK).json({
+            success:true,
+            message:MESSAGES.AUTHOR_GET_ALL,
+            data:author
+        })
+    } catch (error) {
+        res.status(STATUS_CODES.BAD_REQUEST).json({
+            success:false,
+            message:error.message
+        })
+    }
+    
+}
+// async function updateController(req,res){
+//     console.log("Inside getAllAuthorController ");
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// }
+
+module.exports = {createAuthorController,getAllAuthorController}
