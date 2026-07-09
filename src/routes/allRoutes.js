0056f3fb5c882d59cbe8e908ff02a39c1ef8cfd5
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-09 16:14:35 +0530

    feat(auth):add route for author

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const userRoute = require('./userRoute')
+const authorRoute = require('./authorRoute')
 
 // to set up routes outside server
 const router = new express.Router()
 //user authentication 
 router.use('/user',userRoute)
+router.use("/author",authorRoute)
+
 
 module.exports = router
