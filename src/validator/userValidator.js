e3eed5f1e6bb3e88f648834d4f0291438688a88a
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-13 10:01:38 +0530

    style: variable name change to uppercase

--- src/validator/userValidator.js
+++ src/validator/userValidator.js
@@ -1,60 +1,52 @@
-const MESSAGES = require("../constants/messages")
-const {USERTYPES,PASSWORD_MIN_LENGTH, EMAILREGEX} = require("../constants/constants")
-const STATUS_CODES = require("../constants/messages")
+const MESSAGES = require("../constants/messages");
+const {USERTYPES,PASSWORD_MIN_LENGTH,EMAILREGEX} = require("../constants/constants");
+const STATUS_CODES = require("../constants/statusCodes");
 
-function registerValidation(req,res,next){
-    console.log("Inside registervalidation");
-    const {name,role,email,password} = req.body
-    if(!name||!role||!email||!password){
-        return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INCOMPLETE_FORM)
+function registerValidation(req, res, next) {
+  console.log("Inside registervalidation");
+  const { name, role, email, password } = req.body;
+  if (!name || !role || !email || !password) {
+    return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INCOMPLETE_FORM);
+  }
+  //role
+  if (role) {
+    req.body.role = role.toLowerCase();
+    if (!USERTYPES.includes(req.body.role)) {
+      return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.NOT_FOUND);
     }
-    //role
-    if(role){
-        req.body.role = role.toLowerCase()
-        if(!USERTYPES.includes(req.body.role)){
-        return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.NOT_FOUND) 
-        }
-    }
-    //email regex checking 
-    
-    
-    if(!EMAILREGEX.test(email)){
-        return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INVALID_MAIL)
-    }
-    
-    //password validation
-    if(password.length<PASSWORD_MIN_LENGTH){
-        return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INVALID_PASSWORD) 
+  }
+  //email regex checking
 
-    }
+  if (!EMAILREGEX.test(email)) {
+    return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INVALID_MAIL);
+  }
 
-    next()
+  //password validation
+  if (password.length < PASSWORD_MIN_LENGTH) {
+    return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INVALID_PASSWORD);
+  }
+
+  next();
 }
 
-function loginValidation(req,res,next){
-    console.log("inside Login validation");
-    const {email,password}=req.body
-    if(!email||!password){
-        return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INCOMPLETE_FORM)
+function loginValidation(req, res, next) {
+  console.log("inside Login validation");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INCOMPLETE_FORM);
+  }
+  //email regex checking
 
-    }
-     //email regex checking 
-    
-  
-    if(!EMAILREGEX.test(email)){
-        return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INVALID_MAIL)
-    }
-    
-    //password validation
-    if(password.length<6){
-        return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INVALID_PASSWORD) 
-    }
+  if (!EMAILREGEX.test(email)) {
+    return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INVALID_MAIL);
+  }
 
-    next()
+  //password validation
+  if (password.length < 6) {
+    return res.status(STATUS_CODES.BAD_REQUEST).json(MESSAGES.INVALID_PASSWORD);
+  }
 
-
-    
+  next();
 }
 
-
-module.exports = {registerValidation,loginValidation}
+module.exports = { registerValidation, loginValidation };
