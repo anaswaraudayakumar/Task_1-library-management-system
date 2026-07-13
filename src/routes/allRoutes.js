c6fd9b61a29219566cc6e714f9a8c46f84600ccc
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-13 10:21:55 +0530

    chore(cate):update allroutes file with category

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const userRoute = require('./userRoute')
 const authorRoute = require('./authorRoute')
+const catRoute = require('./categoryRoute')
 
 // to set up routes outside server
 const router = new express.Router()
+
 //user authentication 
 router.use('/user',userRoute)
+//author
 router.use("/author",authorRoute)
 
+//category
+router.use("/category",catRoute)
 
 module.exports = router
