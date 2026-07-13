41796cc6c89d2a3644a5dd5eaac2c4593c6e9e6b
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-13 10:10:35 +0530

    feat(cate): add category repository for db operation

---
+++ src/repositories/categoryRepository.js
@@ -0,0 +1,24 @@
+const Category = require("../models/categoryModel")
+
+//create
+async function createCategory(data){
+    return await Category.create(data)
+}
+
+//findAll
+async function findAllCategory(){
+    return await Category.find().select("-__v")
+}
+//find by id 
+async function findOneById(id){
+    const getOne = await Category.findById(id)
+    return getOne
+}
+async function findOneByName(categoryName){
+    const getOne = await Category.findOne({categoryName})
+    return getOne 
+}
+
+
+
+module.exports = {createCategory,findAllCategory,findOneById,findOneByName}
