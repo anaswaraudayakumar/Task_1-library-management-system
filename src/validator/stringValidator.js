373b93c62229e50d0dd041216d0ec6d27af92aea
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-13 09:57:20 +0530

    feat:validate string

---
+++ src/validator/stringValidator.js
@@ -0,0 +1,25 @@
+const MESSAGES = require("../constants/messages");
+const {USERTYPES,PASSWORD_MIN_LENGTH,EMAILREGEX} = require("../constants/constants");
+const STATUS_CODES = require("../constants/statusCodes");
+
+function stringValidator(req,res,next){
+    console.log("Inside stringValidator");
+    for (const key in req.body){
+        const value = req.body[key]
+        if(typeof value !=="string"){
+            res.status(STATUS_CODES.BAD_REQUEST).json({
+            success:false,
+            message: `${key} must be a string`
+        })
+        }
+        if(value.trim ===""){
+           res.status(STATUS_CODES.BAD_REQUEST).json({
+            success:false,
+            message: `${key} could not be empty`   
+        })
+        }
+    }
+    next()
+}
+
+module.exports = stringValidator 
