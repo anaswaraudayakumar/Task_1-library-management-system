f933b36af1adbf5464cd5109e3a6b5df1b9c0ec7
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-13 10:04:37 +0530

    feat(author):add string validator middleware

--- src/routes/authorRoute.js
+++ src/routes/authorRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const adminMiddleware = require('../middleware/adminMiddleware')
 const authorController = require('../controller/authorController')
+const stringValidator = require('../validator/stringValidator')
 
 const authorRoute = express.Router()
 //creating the author 
-authorRoute.post("/authors",adminMiddleware,authorController.createAuthorController)
+authorRoute.post("/authors",adminMiddleware,stringValidator,authorController.createAuthorController)
 //get author
 authorRoute.get("/",adminMiddleware,authorController.getAllAuthorController)
 
