637a2fc9fa0865a4b18e7655edf1a31289ad6508
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-13 10:14:34 +0530

    feat(cate):add category service file for logic operation

---
+++ src/services/categoryService.js
@@ -0,0 +1,26 @@
+const {createCategory,findAllCategory,findOneById,findOneByName} = require("../repositories/categoryRepository");
+
+
+async function createCategoryService(data){
+    const {categoryName,description} = data
+    const existingCategory = await findOneByName(categoryName)
+    if(existingCategory){
+       throw new Error(" Already exist");
+    }
+    const newCategory = await createCategory({categoryName,description})
+    return newCategory
+}
+
+
+async function getAllCategoryService(){
+    console.log("Inside getAllCategory");
+    const allCategory = await findAllCategory()
+    if(!allCategory.length){
+    throw new Error("Category name not listed")
+
+    }
+    return allCategory
+    
+}
+
+module.exports = {createCategoryService,getAllCategoryService}
