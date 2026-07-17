7a6e32d2596eacaf416a73b34704c31902644b0b
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-17 10:19:39 +0530

    feat(book):add book routes in central rute file

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const userRoute = require('./userRoute')
 const authorRoute = require('./authorRoute')
 const catRoute = require('./categoryRoute')
+const bookRoute = require('./bookRoutes')
 
 // to set up routes outside server
 const router = new express.Router()
@@ -13,5 +14,8 @@ router.use("/author",authorRoute)
 
 //category
 router.use("/category",catRoute)
+
+//book
+router.use("/book",bookRoute)
 
 module.exports = router
