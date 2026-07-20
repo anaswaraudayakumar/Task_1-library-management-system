914cb7594b9e25953f838619d7fd7728f76b8b3d
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-20 09:34:23 +0530

    feat(book): new controller function for getting books

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,11 +1,12 @@
-const {createBookService} = require("../services/bookService")
+const {createBookService, getAllBooksService} = require("../services/bookService")
 const MESSAGES = require("../constants/messages");
 const STATUS_CODES = require("../constants/statusCodes");
 
 async function createBookController(req,res){
     console.log("Inside CreateBookController");
+
     try{
-        const newBook = await createBookService(req.body)
+        const newBook = await createBookService(req.body,req.payload)
         res.status(STATUS_CODES.CREATED).json({
             success:true,
             message:MESSAGES.CREATED_SUCCESS,
@@ -19,5 +20,25 @@ async function createBookController(req,res){
     }
     
 }
+async function getAllBookController(req,res){
+    const librarianId = req.payload
+    // console.log(librarianId);
+    
+    try {
+        const books = await getAllBooksService(req.query,librarianId)
+         res.status(STATUS_CODES.OK).json({
+            success:true,
+            message:MESSAGES.GET_ALL,
+            data:books
+        })
+    } catch (error) {
+        res.status(STATUS_CODES.BAD_REQUEST).json({
+            success:false,
+            message:error.message
+        })
+    }
+    
+}
 
-module.exports = {createBookController}
+
+module.exports = {createBookController,getAllBookController}
