03a8a9dab8274e3a31807674a6d68284ab442dc8
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-21 13:49:43 +0530

    feat(book):add logic to get the structured response

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -4,6 +4,7 @@ const {
 } = require('../services/bookService')
 const MESSAGES = require('../constants/messages')
 const STATUS_CODES = require('../constants/statusCodes')
+const { DEFAULT_PAGE, DEFAULT_LIMIT } = require('../constants/constants')
 
 async function createBookController(req, res) {
     console.log('Inside CreateBookController')
@@ -27,11 +28,22 @@ async function getAllBookController(req, res) {
     // console.log(librarianId);
 
     try {
-        const books = await getAllBooksService(req.query, librarianId)
+        const bookData = await getAllBooksService(req.query, librarianId)
+        const books = bookData[0].books
+        const totalBooks = bookData[0].metadata[0]?.totalBooks
+        const page = Number(req.query.page) || DEFAULT_PAGE
+        const limit = Number(req.query.limit) || DEFAULT_LIMIT
+        const totalPages = Math.ceil(totalBooks/limit)
         res.status(STATUS_CODES.OK).json({
             success: true,
             message: MESSAGES.GET_ALL,
             data: books,
+            metadata: {
+                totalBooks,
+                page,
+                limit,
+                totalPages
+            },
         })
     } catch (error) {
         res.status(STATUS_CODES.BAD_REQUEST).json({
