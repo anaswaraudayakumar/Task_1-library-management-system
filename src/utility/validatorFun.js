05c92e1dc1a7f12272e749beb003d961969777d9
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-21 11:18:23 +0530

    feat:validation for query parameters

--- src/utility/validatorFun.js
+++ src/utility/validatorFun.js
@@ -1,43 +1,55 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 function validator(req, schema) {
-  for (const key in req.body) {
-    if (schema[key] === "string") {
-      const error = stringValidator(key, req.body[key]);
-      if (error) return error;
+    for (const key in req.body) {
+        if (schema[key] === 'string') {
+            const error = stringValidator(key, req.body[key])
+            if (error) return error
+        }
+        if (schema[key] === 'number') {
+            const error = numberValidator(key, req.body[key])
+            if (error) return error
+        }
     }
-    if (schema[key] === "number") {
-      const error = numberValidator(key, req.body[key]);
-      if (error) return error;
+    for (const key in req.query) {
+        if (schema[key] === 'id') {
+            const isValid = mongoose.isValidObjectId(req.query[key])
+            if (!isValid) return `${key} havn't valid object id`
+        }
+        if (schema[key] === 'string') {
+            const error = stringValidator(key, req.query[key])
+            if (error) return error
+        }
+        if (schema[key] === 'number') {
+            const error = numberValidator(key, req.query[key])
+            if (error) return error
+        }
     }
-
-    
-  }
-  for (const key in req.query){
-    if (schema[key] === "id") {
-      const isValid = mongoose.isValidObjectId(req.query[key]);
-      if (!isValid) return `${key} is not a valid object id`;
-    }
-  }
-  return null;
+    return null
 }
 
 function stringValidator(key, value) {
-  if (typeof value !== "string") {
-    return `${key} must be a string`;
-  } else {
-    if (value.trim() === "") {
-      return `${key} must have a value`;
+    if (typeof value !== 'string') {
+        return `${key} must be a string`
+    } else {
+        if (value.trim() === '') {
+            return `${key} must have a value`
+        }
     }
-  }
-  return null;
+    return null
 }
 
 function numberValidator(key, value) {
-  if (typeof value !== "number") {
-    return `${key} must have a value`;
-  }
-  return null;
+    if (value === undefined || value === null || value === '') {
+        return `${key} must have a number`
+    }
+
+    const num = Number(value)
+    if (Number.isNaN(num) || !Number.isInteger(num)) {
+        return `${key} must have a valid integer`
+    }
+
+    return null
 }
 
-module.exports = validator;
+module.exports = validator
