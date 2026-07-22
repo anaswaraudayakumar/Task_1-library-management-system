f48e7b120f814dc9925bf020d335208ab232584b
anaswaraudayakumar <anaswarakambaran@gmail.com>
2026-07-22 17:40:21 +0530

    fix : add pagination function and removed unused imports

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,10 +1,13 @@
 const {
     createBookService,
     getAllBooksService,
+    removeBookService,
+    updateBookService,
+    getOneBookService,
 } = require('../services/bookService')
 const MESSAGES = require('../constants/messages')
 const STATUS_CODES = require('../constants/statusCodes')
-const { DEFAULT_PAGE, DEFAULT_LIMIT } = require('../constants/constants')
+const pagination = require('../utility/paginationFun')
 
 async function createBookController(req, res) {
     console.log('Inside CreateBookController')
@@ -31,9 +34,8 @@ async function getAllBookController(req, res) {
         const bookData = await getAllBooksService(req.query, librarianId)
         const books = bookData[0].books
         const totalBooks = bookData[0].metadata[0]?.totalBooks
-        const page = Number(req.query.page) || DEFAULT_PAGE
-        const limit = Number(req.query.limit) || DEFAULT_LIMIT
-        const totalPages = Math.ceil(totalBooks/limit)
+        const { page, limit } = pagination(req.query)
+        const totalPages = Math.ceil(totalBooks / limit)
         res.status(STATUS_CODES.OK).json({
             success: true,
             message: MESSAGES.GET_ALL,
@@ -42,7 +44,7 @@ async function getAllBookController(req, res) {
                 totalBooks,
                 page,
                 limit,
-                totalPages
+                totalPages,
             },
         })
     } catch (error) {
@@ -53,4 +55,64 @@ async function getAllBookController(req, res) {
     }
 }
 
-module.exports = { createBookController, getAllBookController }
+//get one book by id
+async function getOneBookController(req, res) {
+    const librarianId = req.payload
+    const { id } = req.params
+    try {
+        const book = await getOneBookService(id, librarianId)
+        res.status(STATUS_CODES.OK).json({
+            success: true,
+            message: MESSAGES.GET_ALL,
+            data: book,
+        })
+    } catch (error) {
+        res.status(STATUS_CODES.BAD_REQUEST).json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
+async function updateBookcontroller(req, res) {
+    const { id } = req.params
+    const librarianId = req.payload
+    try {
+        const updateBook = await updateBookService(id, librarianId, req.body)
+        res.status(STATUS_CODES.OK).json({
+            success: true,
+            message: MESSAGES.EDIT,
+            data: updateBook,
+        })
+    } catch (error) {
+        res.status(STATUS_CODES.BAD_REQUEST).json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
+async function removeBookController(req, res) {
+    const { id } = req.params
+
+    try {
+        const removeBook = await removeBookService(id)
+        res.status(STATUS_CODES.OK).json({
+            success: true,
+            message: MESSAGES.DELETE,
+            data: removeBook,
+        })
+    } catch (error) {
+        res.status(STATUS_CODES.BAD_REQUEST).json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
+module.exports = {
+    createBookController,
+    getAllBookController,
+    getOneBookController,
+    removeBookController,
+    updateBookcontroller,
+}
